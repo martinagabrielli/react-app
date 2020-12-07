db6c5183290be10db73ca0193e9a2b10111bb164
martinagabrielli <martina.gabrielli@yahoo.it>
2020-12-07 16:32:18 +0000

    added useState and search function with fetch request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 const api = {
   key: "6f330a447effe8dd770d8142aa319c4c",
   base: "https://api.openweathermap.org/data/2.5/"
 }
 
 function App() {
-  
+
+  const [query, setQuery] = useState('')
+  const [weather, setWeather] = useState({})
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => setWeather(result))
+    }
+  }
+
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
