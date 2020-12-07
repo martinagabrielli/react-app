99c36fe4bc5c42eb311639e48daa8f0898b12ef9
martinagabrielli <martina.gabrielli@yahoo.it>
2020-12-07 15:30:43 +0000

    basic app structure

--- src/App.js
+++ src/App.js
@@ -6,7 +6,16 @@ const api = {
 
 function App() {
   return (
-    <div>
+    <div className="app">
+       <main>
+         <div className="search-box">
+           <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+           />
+         </div>
+       </main>
     </div>
   )
 }
