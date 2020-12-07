fbee05f0c2db1646a8428dde54aee1a3b22b4291
martinagabrielli <martina.gabrielli@yahoo.it>
2020-12-07 15:18:44 +0000

    cleaned files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react'
-import Header from './components/Header'
-import Main from './components/Main'
-import todosData from './todosData'
 
 function App() {
-  const todos = todosData.map(todo => <Main key={todo.id} todo={todo.text}/>) 
   return (
-    <>
-      <Header />
-      {todos}
-    </>
+    <div>
+    </div>
   )
 }
 
